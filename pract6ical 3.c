d5f2a0fe6eb939d8e502144b4a1189d1e515f281
Aryashah-ce <aryashahce@gmail.com>
2025-07-22 10:58:06 +0530

    Add files via upload

---
+++ pract6ical 3.c
@@ -0,0 +1,35 @@
+//Coding a BMI calculator
+#include<stdio.h>
+int main()
+{
+    int Weight;
+    float Height;
+    printf("Your Weight(in kilograms):");
+    scanf("%d",&Weight);
+    printf("\nYour Height(in meters)\t :");
+    scanf("%f",&Height);
+    float BMI=Weight/(Height*Height);
+    printf("Your BMI is \t\t :%.2f",BMI);
+    if(BMI<18.5)
+    {
+        printf("\nYour are Under Weight");
+    }
+    else if(BMI>=18.5 & BMI<=24.9)
+    {
+        printf("\nYou are Healthy");
+    }
+    else if(BMI>24.9 & BMI<=29.9 )
+    {
+        printf("\nYour are OverWeight");
+    }
+    else if(BMI>29.9 & BMI<39.9)
+    {
+        printf("\nYou are Obesity");
+    }
+    else
+    {
+        printf("\nYour are having severe Obesity");
+    }
+
+    return 0;
+}
