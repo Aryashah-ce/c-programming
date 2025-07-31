81917e4ad87088ec48bd2c04adc2bafbaddd1d6e
Aryashah-ce <aryashahce@gmail.com>
2025-07-31 16:14:03 +0530

    Add files via upload

---
+++ practical8.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+  int age;
+  printf("Enter your age:");
+  scanf("%d",&age);
+(age>=18)? printf("You are allowed to open bank account"): printf("You are not allowed to open bank account");
+}
